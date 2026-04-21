d9f15f9795392aa251905a76f62b95227f43ca58
Nathaniel Lee <leenathaniel001@gmail.com>
2026-04-20 23:23:08 -0500

    Stuff

--- trade/nothing_config.py
+++ trade/nothing_config.py
@@ -23,6 +23,7 @@ NOTHING_SERIES = [
     ('KXEARNINGSMENTIONUBER', 'Uber earnings mentions',    'mentions'),
     ('KXEARNINGSMENTIONLYFT', 'Lyft earnings mentions',    'mentions'),
     ('KXEARNINGSMENTIONABNB', 'ABNB earnings mentions',    'mentions'),
+    ('KXFEDMENTION', 'Fed meeting mentions', 'mentions')
 
     # Candidates worth evaluating but not added by default:
     # ('KXLABORANNOUNCE',     'Trump Labor Sec announce',  'politics'),  # time-bucket
